30ebdc1457e7a6836d0bd675256575aca84e0730
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-25 15:43:12 -0400

    defined columns in category.js and added comment of site used to help

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER, STRING } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
@@ -7,6 +7,18 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    //used the following link to help find the correct wya do define the columns
+    //https://docs.sqlalchemy.org/en/13/orm/mapping_columns.html
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
