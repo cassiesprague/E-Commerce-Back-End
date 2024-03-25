2f7265c0a7636bfa1e0ce84d37b67ddf9d212852
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-25 16:13:45 -0400

    copied category columns to product columns but changed from category name to product name

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,19 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+     //used the following link to help find the correct wya do define the columns
+    //also looked back at instrucor 05 create
+    //https://docs.sqlalchemy.org/en/13/orm/mapping_columns.html
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
