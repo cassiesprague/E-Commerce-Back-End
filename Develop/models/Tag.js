368e7f1089402678e4a65f243010b0700229713c
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-25 16:39:31 -0400

    added tag id and dag name

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,6 +7,18 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    //used the following link to help find the correct wya do define the columns
+    //also looked back at instrucor 05 create
+    //https://docs.sqlalchemy.org/en/13/orm/mapping_columns.html
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    },
   },
   {
     sequelize,
