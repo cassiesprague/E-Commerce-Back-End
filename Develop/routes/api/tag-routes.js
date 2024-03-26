5711fea021dde29dfca8a6f4d044043fada4de31
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-26 13:02:02 -0400

    used category routes to complete tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,24 +4,60 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  Tag.findAll().then((tagData) => {
+    res.json(tagData);
+  });
   // find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
+  Tag.findById(req.params.id).then((tagData) => {
+    res.json(tagData);
+  });
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
+  Tag.create(req.body)
+  .then((newTag) => {
+    res.json(newTag);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
   // create a new tag
 });
 
 router.put('/:id', (req, res) => {
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((updateTag) => {
+    res.json(updatedTag);
+  })
+  .catch((err) => res.json(err));
+  });
   // update a tag's name by its `id` value
-});
 
 router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedTag) => {
+    res.json(deletedTag);
+  })
+  .catch((err) => res.json(err));
   // delete on tag by its `id` value
 });
 
